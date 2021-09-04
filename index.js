984be524cab3fb8081c46bbb3cddc1e7cd507971
Amitkumar270802 <83585661+Amitkumar270802@users.noreply.github.com>
2021-09-05 01:48:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+function fun1() {
+    window.open('https://amitkumar270802.github.io/calculator.github.io/', '_blank')
+}
+
+function fun2() {
+    window.open('https://amitkumar270802.github.io/unit-convertor.github.io/', '_blank')
+}
+
+function fun3() {
+    window.open('https://amitkumar270802.github.io/portfolio.github.io/', '_blank')
+}
+
+function fun4() {
+    window.open('https://amitkumar270802.github.io/clock.github.io/', '_blank')
+}
+
+function fun5() {
+    window.location = 'https://facebook.com'
+}
+
+function fun6() {
+    window.location = 'https://instagram.com'
+}
+
+function fun7() {
+    window.location = 'https://www.whatsapp.com/'
+}
+
+function fun8() {
+    window.location = 'https://linkedin.com'
+}
+
+function fun9() {
+    window.location = 'https://twitter.com'
+}
+
+function fun10() {
+    window.location = "messanger.com"
+}
